e8b695f51ab367b62fa11e3b8a6383eee10d2508
Szymon Jędrzychowski <Szymon54322@gmail.com>
2025-05-17 01:16:25 +0200

    Created models and connected to databese. First migrations

--- StudyApp/models.py
+++ StudyApp/models.py
@@ -1,3 +1,58 @@
 from django.db import models
+from django.utils import timezone
+from django.db import models
+from django.core.exceptions import ValidationError
+import datetime
 
 # Create your models here.
+
+
+#TODO 1 Add profile model (profile_picture etc)
+
+class Users(models.Model):
+  user_name = models.CharField(max_length=100, unique=True, null=False)
+  created_at = models.DateField(default=datetime.date.today)
+
+  def __str__(self):
+     return self.user_name
+
+class Goals(models.Model):
+  STATUS_CHOICES = [
+        ('New', 'New'),
+        ('In progress', 'In progress'),
+        ('Done', 'Done'),
+    ]
+
+  user = models.ForeignKey(Users, related_name='goals', null=False, on_delete=models.CASCADE)
+  title = models.CharField(max_length=50, default='Untitled Goal')
+  description = models.TextField(max_length=500)
+  status = models.CharField(max_length=15, choices=STATUS_CHOICES, default='New')
+  deadline = models.DateField(null=True, blank=True)
+  created_at = models.DateField(default=datetime.date.today)
+
+  def clean(self):
+    super().clean()
+    if self.deadline and self.deadline < timezone.now().date():
+        raise ValidationError("Deadline cannot be in the past.")
+    
+  def __str__(self):
+     return self.title
+    
+class Tasks(models.Model):
+   goal = models.ForeignKey(Goals, related_name='tasks', null=False, on_delete=models.CASCADE)
+   title = models.CharField(max_length=50, default='Untitled Task')
+   description = models.TextField(max_length=500)
+   is_done = models.BooleanField(default=False)
+   created_at = models.DateField(default=datetime.date.today)
+
+   def __str__(self):
+     return self.title
+
+class Notes(models.Model):
+   user = models.ForeignKey(Users, related_name='notes', null=False, on_delete=models.CASCADE)
+   goal = models.ForeignKey(Goals, related_name='notes', null=True, blank=True, on_delete=models.CASCADE)
+   text = models.TextField(max_length=500)
+   created_at = models.DateField(default=datetime.date.today)
+
+   def __str__(self):
+     return self.text
--- StudyBuddy/settings.py
+++ StudyBuddy/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+from pathlib import Path
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qc!!u*z69$^2i68k+#8!apuu-f(smtrlxg55u_j=&7elc@^yzc'
+SECRET_KEY = os.getenv('DJANGO_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -77,8 +81,12 @@ WSGI_APPLICATION = 'StudyBuddy.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': os.getenv('ENGINE'),
+        'NAME': os.getenv('NAME'),
+        'USER': os.getenv('USER'),
+        'PASSWORD': os.getenv('PASSWORD'),
+        'HOST': os.getenv('HOST'),
+        'PORT': os.getenv('PORT')
     }
 }
 
