4c5d07eacb739e528925cf9a1c21f95b6fd041ee
Szymon Jędrzychowski <Szymon54322@gmail.com>
2025-05-17 16:41:34 +0200

    Added starting page with goals

--- StudyApp/admin.py
+++ StudyApp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from . models import Users, Goals, Tasks, Notes
 
 # Register your models here.
+
+admin.site.register(Users)
+admin.site.register(Goals)
+admin.site.register(Tasks)
+admin.site.register(Notes)
--- StudyApp/views.py
+++ StudyApp/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from django.views import View
+from .models import Goals
 
 # Create your views here.
 
 class HomeView(View):
 
   def get(self, request):
-    return render(request, 'StudyApp/index.html')
+    goals = Goals.objects.all()
+    return render(request, 'StudyApp/index.html', {
+      'goals':goals
+    })
